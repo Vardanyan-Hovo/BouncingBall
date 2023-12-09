46e6834db22e8067db07a9b90dbfbcb6e4f91657
Vardanyan-Hovo <hovhannes_vardanyan1@mail.ru>
2023-12-10 03:20:16 +0400

    start

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+"use strict";
+console.log("Hello");
+let age = 20;
+if (age < 50)
+    age += 10;
+console.log(age);
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+const greeting = greet("TypeScript + asdasdasd +vvhvj");
+console.log(greeting);
+//# sourceMappingURL=index.js.map
---
+++ src/index.ts
@@ -0,0 +1,13 @@
+console.log("Hello")
+let age: number = 20;
+
+if(age < 50)
+    age += 10;
+console.log(age );
+
+function greet(name: string): string
+{
+    return `Hello, ${name}!`;
+}
+const greeting = greet("HHHHTypeScripthoihoihoi + asdasdasd +vvhvj");
+console.log(greeting);
